47fe2b5a14ce8db910b05befbd385cbe517b1707
antopix <antopix33@gmail.com>
2025-01-29 18:38:02 -0800

    hover effect, added

--- script.js
+++ script.js
@@ -8,9 +8,13 @@ square.classList.add("square");
 function createSquare() {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseover", handleSquareHover);
   return square;
 }
 
+function handleSquareHover(event) {
+  event.target.style.backgroundColor = 'red';
+}
 
 for (let i = 0; i < 256; i++) {
   container.appendChild(createSquare());
